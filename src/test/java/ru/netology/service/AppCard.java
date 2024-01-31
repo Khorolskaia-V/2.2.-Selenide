73cf27f9d9ca9a983a2d510d80ae57c620e3a62e
Виктория <viksyaavito@gmail.com>
2024-01-31 23:17:25 +0300

    positive test

---
+++ src/test/java/ru/netology/service/AppCard.java
@@ -0,0 +1,59 @@
+package ru.netology.service;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.commands.ShouldBe;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class AppCard {
+
+    @Test
+    void shoulTest() throws InterruptedException {
+        int interval = 1000;
+
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Ярославль");
+        Thread.sleep(interval);
+        $("[data-test-id='date'] button").click();
+        Thread.sleep(interval);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        Thread.sleep(interval);
+        String date = LocalDate
+                .now()
+                .plusDays(4)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='date'] input").setValue(date);
+        Thread.sleep(interval);
+        $("[data-test-id='name'] input").setValue("Мягкие Лапки");
+        Thread.sleep(interval);
+        $("[data-test-id='phone'] input").setValue("+79118226633");
+        Thread.sleep(interval);
+        $("[data-test-id='agreement']").click();
+        Thread.sleep(interval);
+        $(".button_view_extra").click();
+        Thread.sleep(interval);
+        $("[data-test-id='notification'] .notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
+        Thread.sleep(interval);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
